17d9f091ef14e3892d22a494d8420430db63239f
Mathias Dalheimer <md@gonium.net>
2012-08-05 14:30:53 +0200

    working hack: runs $HOME/bin/fc-run.sh after each completed race.

--- src/game_over.cpp
+++ src/game_over.cpp
@@ -47,225 +47,263 @@
 
 #include "bench.h"
 
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <cstdlib>
+
+std::string exec(std::string cmd) {
+    char buffer[128];
+    FILE* pipe = popen(cmd.c_str(), "r");
+    if (!pipe) {
+      std::ostringstream oss;
+      oss << "ERROR: cannot execute command ";
+      oss << cmd;
+      return oss.str();
+    }
+    std::ostringstream oss;
+    while(!feof(pipe)) {
+      if(fgets(buffer, 128, pipe) != NULL)
+        oss << buffer;
+    }
+    pclose(pipe);
+    return oss.str();
+}
+
 GameOver::GameOver()
 {
-	halt_sound( "flying_sound" );
-    halt_sound( "rock_sound" );
-    halt_sound( "ice_sound" );
-    halt_sound( "snow_sound" );
+  halt_sound( "flying_sound" );
+  halt_sound( "rock_sound" );
+  halt_sound( "ice_sound" );
+  halt_sound( "snow_sound" );
+  char * home_path;
+  home_path = getenv ("HOME");
+  if (home_path!=NULL) {
+    // Patch MD: Callback for fullcircle
+    std::cout << "GameOver: Executing fullcircle toolchain" << std::endl;
+    int minutes, seconds, hundredths;
+    getTimeComponents( gameMgr->time, minutes, seconds, hundredths );
+    std::ostringstream oss;
+    oss << home_path << "/bin/fc-run.sh";
+    oss << " " << minutes << "a" << seconds << "b" << hundredths;
+    std::string command=oss.str();
+    std::cout << "Running " << command << std::endl;
+    std::string run_log=exec(command);
+    std::cout << "Log was: " << run_log << std::endl;
+  } else {
+    std::cout << "No HOME environment variable found - cannot locate fc-run.sh" << std::endl;
+  }
+  if( Benchmark::getMode() != Benchmark::NONE ){
+    Benchmark::displayState();
+    winsys_exit( 0 );
+  }
 
-    std::cout << "GameOver: Constructor" << std::endl;
+  play_music( "game_over" );
 
-	if( Benchmark::getMode() != Benchmark::NONE ){
-		Benchmark::displayState();
-		winsys_exit( 0 );
-	}
-		
-    play_music( "game_over" );
+  m_aborted = gameMgr->wasRaceAborted();
 
-    m_aborted = gameMgr->wasRaceAborted();
+  if ( !m_aborted ) {
+    gameMgr->updatePlayersScores();
+  }
 
-	if ( !m_aborted ) {
-		gameMgr->updatePlayersScores();
+  if ( gameMgr->gametype!=GameMgr::PRACTICING ) {
+    m_bestScore = gameMgr->updateCurrentRaceData();		
+
+    if(!gameMgr->wasRaceWon()){
+      players[0].decLives();
     }
-		
-    if ( gameMgr->gametype!=GameMgr::PRACTICING ) {
-		m_bestScore = gameMgr->updateCurrentRaceData();		
-		
-		if(!gameMgr->wasRaceWon()){
-			players[0].decLives();
-		}
-    }else{
-		if ( !m_aborted ) {
-			m_bestScore = players[0].updateOpenCourseData(
-									gameMgr->getCurrentRace().name,
-									gameMgr->time,
-									players[0].herring,
-									players[0].score);
-			
-			if(m_bestScore) players[0].saveData();
-		}
-	}
-	
-	{  
-	pp::Vec3d dir = players[0].vel;
-	int speed = int(dir.normalize());
-	//set max_speed
-	if (speed > players[0].max_speed) players[0].max_speed=int(speed);
-	}
-	
-	int width = getparam_x_resolution();
-    int height = getparam_y_resolution();
-	
-	pp::Vec2d pos(width/2, height/2 +200);
-	
-	if ( gameMgr->wasRaceAborted() ) {
-		mp_raceOverLbl = new pp::Label(pos,"race_over",_("Race aborted"));
-		mp_raceOverLbl->alignment.center();
-    }else{	
-		mp_raceOverLbl = new pp::Label(pos,"race_over",_("Race Over"));
-		mp_raceOverLbl->alignment.center();
-	
-		char buff[BUFF_LEN];
-		int minutes, seconds, hundredths;
+  }else{
+    if ( !m_aborted ) {
+      m_bestScore = players[0].updateOpenCourseData(
+          gameMgr->getCurrentRace().name,
+          gameMgr->time,
+          players[0].herring,
+          players[0].score);
 
-		getTimeComponents( gameMgr->time, minutes, seconds, hundredths );
-		sprintf( buff, _("Time: %02d:%02d.%02d"), minutes, seconds, hundredths );	
-		pos.y-=100;
-		mp_timeLbl = new pp::Label(pos,"race_stats", buff);
-		mp_timeLbl->alignment.center();
-	
-		sprintf( buff, _("Herring: %3d"), players[0].herring );
-		pos.y-=30;
-		mp_herringLbl = new pp::Label(pos,"race_stats",buff);
-		mp_herringLbl->alignment.center();
-	
-		sprintf( buff, _("Score: %6d"), players[0].score );
-		pos.y-=30;
-		mp_scoreLbl = new pp::Label(pos,"race_stats",buff);
-		mp_scoreLbl->alignment.center();
-	
-		int speed = int((double)players[0].max_speed * M_PER_SEC_TO_KM_PER_H);
-		sprintf( buff, _("Max speed: %3d km/h"), speed);
-		pos.y-=30;
-		mp_maxspeedLbl = new pp::Label(pos,"race_stats",buff);
-		mp_maxspeedLbl->alignment.center();
-	
-		double percent = (gameMgr->airbornetime / gameMgr->time) * 100.0;
-		sprintf( buff, _("Was flying: %.01f %% of time"), percent);
-		pos.y-=30;
-		mp_flyingLbl = new pp::Label(pos,"race_stats",buff);
-		mp_flyingLbl->alignment.center();
-		
-		char buff2[50];
-		snprintf(buff2, 50, "");
-		if(highscore::useHighscore) {
-			int pos = Highscore->addScore(gameMgr->getCurrentRace().name,players[0].name,players[0].score);
-			if(pos != -1)
-				snprintf(buff2, 50, _("You made it to the %s place in the highscore!"),highscore::posToStr(pos).c_str());
-		}
-		pos.y-=30;
-		mp_highscoreLbl = new pp::Label(pos,"race_stats",buff2);
-		mp_highscoreLbl->alignment.center();
-	
-		const char *string="";
-	
-		if ( gameMgr->gametype==GameMgr::PRACTICING){
-			if(m_bestScore){
-				string = _("You beat your best score!");
-			}
-		} else if(gameMgr->wasEventWon()){
-			string = _("Congratulations! You won the event!");
-		} else if(gameMgr->wasCupWon()){
-			string = _("Congratulations! You won the cup!");
-		} else if(gameMgr->wasRaceWon()){
-			string = _("You advanced to the next race!");
-		} else {
-			string = _("You didn't advance.");
-		}	
-	
-		pos.y-=30;
-		mp_resultsLbl = new pp::Label(pos,"race_stats",string);
-		mp_resultsLbl->alignment.center();
-	}
+      if(m_bestScore) players[0].saveData();
+    }
+  }
+
+  {  
+    pp::Vec3d dir = players[0].vel;
+    int speed = int(dir.normalize());
+    //set max_speed
+    if (speed > players[0].max_speed) players[0].max_speed=int(speed);
+  }
+
+  int width = getparam_x_resolution();
+  int height = getparam_y_resolution();
+
+  pp::Vec2d pos(width/2, height/2 +200);
+
+  if ( gameMgr->wasRaceAborted() ) {
+    mp_raceOverLbl = new pp::Label(pos,"race_over",_("Race aborted"));
+    mp_raceOverLbl->alignment.center();
+  }else{	
+    mp_raceOverLbl = new pp::Label(pos,"race_over",_("Race Over"));
+    mp_raceOverLbl->alignment.center();
+
+    char buff[BUFF_LEN];
+    int minutes, seconds, hundredths;
+
+    getTimeComponents( gameMgr->time, minutes, seconds, hundredths );
+    sprintf( buff, _("Time: %02d:%02d.%02d"), minutes, seconds, hundredths );	
+    pos.y-=100;
+    mp_timeLbl = new pp::Label(pos,"race_stats", buff);
+    mp_timeLbl->alignment.center();
+
+    sprintf( buff, _("Herring: %3d"), players[0].herring );
+    pos.y-=30;
+    mp_herringLbl = new pp::Label(pos,"race_stats",buff);
+    mp_herringLbl->alignment.center();
+
+    sprintf( buff, _("Score: %6d"), players[0].score );
+    pos.y-=30;
+    mp_scoreLbl = new pp::Label(pos,"race_stats",buff);
+    mp_scoreLbl->alignment.center();
+
+    int speed = int((double)players[0].max_speed * M_PER_SEC_TO_KM_PER_H);
+    sprintf( buff, _("Max speed: %3d km/h"), speed);
+    pos.y-=30;
+    mp_maxspeedLbl = new pp::Label(pos,"race_stats",buff);
+    mp_maxspeedLbl->alignment.center();
+
+    double percent = (gameMgr->airbornetime / gameMgr->time) * 100.0;
+    sprintf( buff, _("Was flying: %.01f %% of time"), percent);
+    pos.y-=30;
+    mp_flyingLbl = new pp::Label(pos,"race_stats",buff);
+    mp_flyingLbl->alignment.center();
+
+    char buff2[50];
+    snprintf(buff2, 50, "");
+    if(highscore::useHighscore) {
+      int pos = Highscore->addScore(gameMgr->getCurrentRace().name,players[0].name,players[0].score);
+      if(pos != -1)
+        snprintf(buff2, 50, _("You made it to the %s place in the highscore!"),highscore::posToStr(pos).c_str());
+    }
+    pos.y-=30;
+    mp_highscoreLbl = new pp::Label(pos,"race_stats",buff2);
+    mp_highscoreLbl->alignment.center();
+
+    const char *string="";
+
+    if ( gameMgr->gametype==GameMgr::PRACTICING){
+      if(m_bestScore){
+        string = _("You beat your best score!");
+      }
+    } else if(gameMgr->wasEventWon()){
+      string = _("Congratulations! You won the event!");
+    } else if(gameMgr->wasCupWon()){
+      string = _("Congratulations! You won the cup!");
+    } else if(gameMgr->wasRaceWon()){
+      string = _("You advanced to the next race!");
+    } else {
+      string = _("You didn't advance.");
+    }	
+
+    pos.y-=30;
+    mp_resultsLbl = new pp::Label(pos,"race_stats",string);
+    mp_resultsLbl->alignment.center();
+  }
 }
 
 GameOver::~GameOver()
 {
-	delete mp_raceOverLbl;
-	
-	if ( !gameMgr->wasRaceAborted() ) {
-		delete mp_timeLbl;
-		delete mp_herringLbl;
-		delete mp_scoreLbl;
-		delete mp_maxspeedLbl;
-		delete mp_flyingLbl;
-		delete mp_resultsLbl;
-		delete mp_highscoreLbl;
-	}
+  delete mp_raceOverLbl;
+
+  if ( !gameMgr->wasRaceAborted() ) {
+    delete mp_timeLbl;
+    delete mp_herringLbl;
+    delete mp_scoreLbl;
+    delete mp_maxspeedLbl;
+    delete mp_flyingLbl;
+    delete mp_resultsLbl;
+    delete mp_highscoreLbl;
+  }
 }
 
-void
+  void
 GameOver::loop(float timeStep)
 {
-    int width, height;
-    width = getparam_x_resolution();
-    height = getparam_y_resolution();
+  int width, height;
+  width = getparam_x_resolution();
+  height = getparam_y_resolution();
 
-    /* Check joystick */
-    if ( is_joystick_active() ) {
-	update_joystick();
+  /* Check joystick */
+  if ( is_joystick_active() ) {
+    update_joystick();
 
-	if ( is_joystick_continue_button_down() )
-	{
-	    if ( gameMgr->gametype != GameMgr::PRACTICING ) {
-			set_game_mode( EVENT_RACE_SELECT );
-		}else{
-			set_game_mode( RACE_SELECT );
-		}
-	    winsys_post_redisplay();
-	    return;
-	}
+    if ( is_joystick_continue_button_down() )
+    {
+      if ( gameMgr->gametype != GameMgr::PRACTICING ) {
+        set_game_mode( EVENT_RACE_SELECT );
+      }else{
+        set_game_mode( RACE_SELECT );
+      }
+      winsys_post_redisplay();
+      return;
     }
-    fpsCounter.update();
+  }
+  fpsCounter.update();
 
-    update_audio();
+  update_audio();
 
-    clear_rendering_context();
+  clear_rendering_context();
 
-    fogPlane.setup();
-    update_player_pos( players[0], 0 );
-    update_view( players[0], 0 );
+  fogPlane.setup();
+  update_player_pos( players[0], 0 );
+  update_view( players[0], 0 );
 
-    setup_view_frustum( players[0], NEAR_CLIP_DIST, 
-			getparam_forward_clip_distance() );
+  setup_view_frustum( players[0], NEAR_CLIP_DIST, 
+      getparam_forward_clip_distance() );
 
-    draw_sky(players[0].view.pos);
-    draw_fog_plane();
+  draw_sky(players[0].view.pos);
+  draw_fog_plane();
 
-    set_course_clipping( true );
-    set_course_eye_point( players[0].view.pos );
-    setup_course_lighting();
-    render_course();
-    draw_trees();
-    if ( getparam_draw_particles() ) {
-		draw_particles( players[0] );
-    }
+  set_course_clipping( true );
+  set_course_eye_point( players[0].view.pos );
+  setup_course_lighting();
+  render_course();
+  draw_trees();
+  if ( getparam_draw_particles() ) {
+    draw_particles( players[0] );
+  }
 
-    ModelHndl->draw_tux();
-    draw_tux_shadow();
-    set_gl_options( GUI );
+  ModelHndl->draw_tux();
+  draw_tux_shadow();
+  set_gl_options( GUI );
 
-    UIMgr.setupDisplay();
-	UIMgr.draw();
-		
-    HUD1.draw(players[0]);
-    reshape( width, height );
+  UIMgr.setupDisplay();
+  UIMgr.draw();
 
-    winsys_swap_buffers();
+  HUD1.draw(players[0]);
+  reshape( width, height );
+
+  winsys_swap_buffers();
 }
 
-bool
+  bool
 GameOver::mouseButtonEvent(int button, int x, int y, bool pressed)
 
 {
-	if ( gameMgr->gametype!=GameMgr::PRACTICING ) {
-		set_game_mode( EVENT_RACE_SELECT );
-	}else{
-		set_game_mode( RACE_SELECT );
-	}
-	winsys_post_redisplay();
-	return true;
+  if ( gameMgr->gametype!=GameMgr::PRACTICING ) {
+    set_game_mode( EVENT_RACE_SELECT );
+  }else{
+    set_game_mode( RACE_SELECT );
+  }
+  winsys_post_redisplay();
+  return true;
 }
 
-bool
+  bool
 GameOver::keyPressEvent(SDLKey key)
 {
-	if ( gameMgr->gametype!=GameMgr::PRACTICING ) {
-		set_game_mode( EVENT_RACE_SELECT );
-	}else{
-		set_game_mode( RACE_SELECT );
-	}
-	winsys_post_redisplay();
-	return true;
+  if ( gameMgr->gametype!=GameMgr::PRACTICING ) {
+    set_game_mode( EVENT_RACE_SELECT );
+  }else{
+    set_game_mode( RACE_SELECT );
+  }
+  winsys_post_redisplay();
+  return true;
 }
