3e69b5dd77c7f1cdd85f67c5c62fcc468ba4fb67
Mathias Dalheimer <md@gonium.net>
2012-08-07 01:22:19 +0200

    patched loop to repeat the level first selected all over

--- src/loop.cpp
+++ src/loop.cpp
@@ -68,115 +68,122 @@ void set_game_mode( game_mode_t mode )
 
 void main_loop()
 {
-	if ( getparam_capture_mouse() ) {
-		const int w = getparam_x_resolution();
-		const int h = getparam_y_resolution();
-		pp::Vec2d pos = UIMgr.getMousePosition();
+  if ( getparam_capture_mouse() ) {
+    const int w = getparam_x_resolution();
+    const int h = getparam_y_resolution();
+    pp::Vec2d pos = UIMgr.getMousePosition();
 
-		// Flip y coordinates
-		pos.y = h - pos.y;
+    // Flip y coordinates
+    pos.y = h - pos.y;
 
-		if ( pos.x < 0 ) {
-		    pos.x = 0;
-		}
-		if ( pos.x > w-1 ) {
-		    pos.x = w-1;
-		}
-		if ( pos.y < 0 ) {
-		    pos.y = 0;
-		}
-		if ( pos.y > h-1 ) {
-		    pos.y = h-1;
-		}
-
-		winsys_warp_pointer( int(pos.x), int(pos.y) );
+    if ( pos.x < 0 ) {
+      pos.x = 0;
     }
-	
-	if ( GameMode::mode != new_mode ) {
-		
-		if (GameMode::currentMode!=NULL){
-			delete GameMode::currentMode;
-		}
-		GameMode::prevmode = GameMode::mode;
+    if ( pos.x > w-1 ) {
+      pos.x = w-1;
+    }
+    if ( pos.y < 0 ) {
+      pos.y = 0;
+    }
+    if ( pos.y > h-1 ) {
+      pos.y = h-1;
+    }
 
-		switch(new_mode){
-			case SPLASH:
-				GameMode::currentMode = new SplashScreen();
-				break;			
-			case GAME_TYPE_SELECT:
-				GameMode::currentMode = new GameTypeSelect();
-				break;
-			case CREDITS:
-				GameMode::currentMode = new Credits();
-				break;
-			case CONFIGURATION:
-				GameMode::currentMode = new Configuration();
-				break;
-			case RACE_SELECT:
-				GameMode::currentMode = new RaceSelect();
-				break;
-			case LOADING:
-				GameMode::currentMode = new Loading();
-				break;
-			case INTRO:
-				GameMode::currentMode = new Intro();
-				break;
-			case RACING:
-				GameMode::currentMode = new Racing();
-				break;
-			case PAUSED:
-				GameMode::currentMode = new Paused();
-				break;
-			case GAME_OVER:
-				GameMode::currentMode = new GameOver();
-				break;			
-			case RESET:
-				GameMode::currentMode = new Reset();
-				break;
-			case EVENT_SELECT:
-				GameMode::currentMode = new EventSelect();
-				break;
-			case CONFIG_GRAPHICS:
-				GameMode::currentMode = new GraphicsConfig();
-				break;
-			case CONFIG_VIDEO:
-				GameMode::currentMode = new VideoConfig();
-				break;
-			case CONFIG_AUDIO:
-				GameMode::currentMode = new AudioConfig();
-				break;
-			case CONFIG_KEYBOARD:
-				GameMode::currentMode = new KeyboardConfig();
-				break;
-			case CONFIG_JOYSTICK:
-				GameMode::currentMode = new JoystickConfig();
-				break;
-			case EVENT_RACE_SELECT:
-				GameMode::currentMode = new EventRaceSelect();
-				break;
-			case BENCHMARK:
-				GameMode::currentMode = new Benchmark();
-			case HIGHSCORE:
-				GameMode::currentMode = new HighscoreShow();
-				break;
-			default:{}
-				//todo: add fallback			
-		}
-		
-		GameMode::mode = new_mode;
-	
-		// Reset time step clock so that there isn't a sudden
-		//  jump when we start the new mode 
-		gameMgr->resetTimeStep();
-	}
-	
-	gameMgr->updateTimeStep();
-	
-	if ( Benchmark::getTimeStep() >0.0 ){
-		GameMode::currentMode->loop(Benchmark::getTimeStep());
-	}else{	
-		GameMode::currentMode->loop( gameMgr->getTimeStep() );
-	}
+    winsys_warp_pointer( int(pos.x), int(pos.y) );
+  }
+
+  if ( GameMode::mode != new_mode ) {
+
+    if (GameMode::currentMode!=NULL){
+      delete GameMode::currentMode;
+    }
+    GameMode::prevmode = GameMode::mode;
+
+    std::cout << "Game mode is: " << new_mode << std::endl;
+
+    if (GameMode::prevmode == GAME_OVER) {
+      std::cout << "Game over detected - setting new mode to LOADING" << std::endl;
+      new_mode = LOADING;
+    }
+
+    switch(new_mode){
+      case SPLASH:
+        GameMode::currentMode = new SplashScreen();
+        break;			
+      case GAME_TYPE_SELECT:
+        GameMode::currentMode = new GameTypeSelect();
+        break;
+      case CREDITS:
+        GameMode::currentMode = new Credits();
+        break;
+      case CONFIGURATION:
+        GameMode::currentMode = new Configuration();
+        break;
+      case RACE_SELECT:
+        GameMode::currentMode = new RaceSelect();
+        break;
+      case LOADING:
+        GameMode::currentMode = new Loading();
+        break;
+      case INTRO:
+        GameMode::currentMode = new Intro();
+        break;
+      case RACING:
+        GameMode::currentMode = new Racing();
+        break;
+      case PAUSED:
+        GameMode::currentMode = new Paused();
+        break;
+      case GAME_OVER:
+        GameMode::currentMode = new GameOver();
+        break;			
+      case RESET:
+        GameMode::currentMode = new Reset();
+        break;
+      case EVENT_SELECT:
+        GameMode::currentMode = new EventSelect();
+        break;
+      case CONFIG_GRAPHICS:
+        GameMode::currentMode = new GraphicsConfig();
+        break;
+      case CONFIG_VIDEO:
+        GameMode::currentMode = new VideoConfig();
+        break;
+      case CONFIG_AUDIO:
+        GameMode::currentMode = new AudioConfig();
+        break;
+      case CONFIG_KEYBOARD:
+        GameMode::currentMode = new KeyboardConfig();
+        break;
+      case CONFIG_JOYSTICK:
+        GameMode::currentMode = new JoystickConfig();
+        break;
+      case EVENT_RACE_SELECT:
+        GameMode::currentMode = new EventRaceSelect();
+        break;
+      case BENCHMARK:
+        GameMode::currentMode = new Benchmark();
+      case HIGHSCORE:
+        GameMode::currentMode = new HighscoreShow();
+        break;
+      default:{}
+              //todo: add fallback			
+    }
+
+    GameMode::mode = new_mode;
+
+    // Reset time step clock so that there isn't a sudden
+    //  jump when we start the new mode 
+    gameMgr->resetTimeStep();
+  }
+
+  gameMgr->updateTimeStep();
+
+  if ( Benchmark::getTimeStep() >0.0 ){
+    GameMode::currentMode->loop(Benchmark::getTimeStep());
+  }else{	
+    GameMode::currentMode->loop( gameMgr->getTimeStep() );
+  }
 }
 
 /*---------------------------------------------------------------------------*/
@@ -184,62 +191,62 @@ void main_loop()
   Returns true if a mode change will occur the next time main_loop() runs.
 
   \author  jfpatry
-*/
+ */
 bool is_mode_change_pending()
 {
-    return bool(GameMode::mode != new_mode);
+  return bool(GameMode::mode != new_mode);
 }
 
 GameMode::GameMode()
 {
 }
 
-	
-void
+
+  void
 GameMode::drawSnow( float timeStep, bool windy )
 {
-	// check wether ui snow is 
-	if(getparam_ui_snow()){
-		// update and draw snow
-		update_ui_snow( timeStep, windy );
-		draw_ui_snow();
-    }
+  // check wether ui snow is 
+  if(getparam_ui_snow()){
+    // update and draw snow
+    update_ui_snow( timeStep, windy );
+    draw_ui_snow();
+  }
 }
 
 void loop_mouse_func (int button, int state, int x, int y)
 {
-	if(GameMode::currentMode!=NULL){
-		bool pressed = state && SDL_PRESSED;
-		if(GameMode::currentMode->mouseButtonEvent(button,x,y,pressed)) return;
-		else if(pressed){
-			if(GameMode::currentMode->mouseButtonPressEvent(button,x,y)) return;
-		}else{
-			if(GameMode::currentMode->mouseButtonReleaseEvent(button,x,y)) return;
-		}	
-		UIMgr.mouseEvent(button,state,x,y);
-	}
+  if(GameMode::currentMode!=NULL){
+    bool pressed = state && SDL_PRESSED;
+    if(GameMode::currentMode->mouseButtonEvent(button,x,y,pressed)) return;
+    else if(pressed){
+      if(GameMode::currentMode->mouseButtonPressEvent(button,x,y)) return;
+    }else{
+      if(GameMode::currentMode->mouseButtonReleaseEvent(button,x,y)) return;
+    }	
+    UIMgr.mouseEvent(button,state,x,y);
+  }
 }
 
 void loop_mouse_motion_func( int x, int y )
 {
-	UIMgr.motionEvent(x,y);
+  UIMgr.motionEvent(x,y);
 }
 
 void loop_keyboard_func(SDLKey key, SDLMod mod, bool release, int x, int y)
 {
-	if(GameMode::currentMode!=NULL){		
-		if (key < SDLK_UP) {
-            if ( isalpha( key ) ) {
-                key = SDLKey(tolower( key ));
-            }
-        }
-		
-		if(GameMode::currentMode->keyboardEvent(key,release)) return;
-		else if(release){
-			if(GameMode::currentMode->keyReleaseEvent(key)) return;
-		}else{
-			if(GameMode::currentMode->keyPressEvent(key)) return;
-		}
-		UIMgr.keyboardEvent(key,mod,release);
-	}
+  if(GameMode::currentMode!=NULL){		
+    if (key < SDLK_UP) {
+      if ( isalpha( key ) ) {
+        key = SDLKey(tolower( key ));
+      }
+    }
+
+    if(GameMode::currentMode->keyboardEvent(key,release)) return;
+    else if(release){
+      if(GameMode::currentMode->keyReleaseEvent(key)) return;
+    }else{
+      if(GameMode::currentMode->keyPressEvent(key)) return;
+    }
+    UIMgr.keyboardEvent(key,mod,release);
+  }
 }
